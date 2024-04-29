083e874bffb659bef09e484ab1c9dab074189f42
dhay salih <dhaysalih18@gmail.com>
2024-04-29 11:48:50 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <select v-model="componentName">
+    <option value="Home">home</option>
+    <option value="About">about</option>
+
+  </select>
+<!--this acts as a placeholder for the rendered component when toggling, we need to bind it -->
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
+
+</template>
+<script>
+import Home from "./components/Home.vue"
+import About from "./components/About.vue"
+
+export default {
+  name: "App",
+  components: {
+    Home, About
+  },
+  data() {
+    return {
+      componentName: "Home",
+    }
+  }
+};
+
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>home section</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("unmount")
+  },
+  activated() {
+    console.log("active")
+  },
+  deactivated() {
+    console.log("idle")
+
+  }
+}
+
+</script>
+
